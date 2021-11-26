cccbf9d156a5c1418ae36f791c5f1d41bbad38c6
nadeem-ninja <nadeemansari.vi@gmail.com>
2021-11-26 17:42:45 +0530

    first commit

---
+++ polls/rough.py
@@ -0,0 +1,38 @@
+<script></script>
+
+
+
+<!--	$.ajax({-->
+<!--		method:'GET',-->
+<!--		url:dataURL,-->
+<!--		success:function(response){-->
+<!--			console.log('RESPONSE:',response)-->
+<!--			for (var i in response){-->
+<!--				var key = object.keys(response[i])[0]-->
+<!--				var value = object.values(response[i])[0]-->
+<!--				state.items.push(key)-->
+<!--				state.values.push(value)-->
+<!--			}-->
+
+<!--	})-->
+
+<!--		console.log('STATE:', state)-->
+<!--		buildChart()-->
+
+<!--function buildChart(){-->
+
+		"scale-x":{
+<!--			"values":state.items-->
+<!--		},-->
+
+<!--		"series":[-->
+<!--		{-->
+<!--			 "values":state.values-->
+
+<!--			}-->
+
+<!--		]-->
+<!--	  }-->
+<!--
+
+<!--	}-->
